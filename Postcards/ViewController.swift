79b0893b9a9de7b8b7dd463907c2c7dc669d1c4c
Brent Robertson <tattooedmac01@me.com>
2015-02-24 19:05:11 +0800

    added comment to check remote update

--- Postcards/ViewController.swift
+++ Postcards/ViewController.swift
@@ -31,6 +31,7 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         //Code will evaluate when we press the button.
         //Adding a comment here to test github.
+        //Comment to check my remote updating.
         
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
